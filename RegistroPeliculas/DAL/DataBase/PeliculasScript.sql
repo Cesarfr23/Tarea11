7ebbbefe881bff78782ef107a503620e1d855c1c
Cesarfr23 <cesarfernandez860@gmail.com>
2016-07-22 20:10:53 +0100

    DataBase

---
+++ RegistroPeliculas/DAL/DataBase/PeliculasScript.sql
@@ -0,0 +1,25 @@
+﻿create DataBase Peliculas;
+go
+
+use Peliculas;
+go
+
+create table Categoria
+(
+	CategoriaId int primary key identity(1,1),
+	Descripcion varchar(50)
+)
+go
+
+create table Pelicula
+(
+	PeliculaId int primary key identity(1,1),
+	Titulo varchar(40),
+	Signosis varchar(100),
+	Calificacion int,
+	Anno int,
+	Idioma varchar(20),
+	Genero varchar(20),
+	CategoriaId int References Categoria (CategoriaId)
+)
+go
