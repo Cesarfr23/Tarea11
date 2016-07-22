bde5f22781a67a18946fa8f6344f54eca6525247
Cesarfr23 <cesarfernandez860@gmail.com>
2016-07-22 12:56:45 +0100

    Agregando metodos a los botones

--- RegistroPeliculas/RegistroPeliculas/RegistroPelicula.Designer.cs
+++ RegistroPeliculas/RegistroPeliculas/RegistroPelicula.Designer.cs
@@ -37,14 +37,14 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.PeliculasIdTextBox = new System.Windows.Forms.TextBox();
+            this.TituloTextBox = new System.Windows.Forms.TextBox();
+            this.SignosisTextBox = new System.Windows.Forms.TextBox();
+            this.CalificacionTextBox = new System.Windows.Forms.TextBox();
+            this.IdiomaTextBox = new System.Windows.Forms.TextBox();
+            this.GeneroTextBox = new System.Windows.Forms.TextBox();
+            this.AnnoComboBox = new System.Windows.Forms.ComboBox();
+            this.CategoriaIdComboBox = new System.Windows.Forms.ComboBox();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -134,63 +134,64 @@
             this.label9.TabIndex = 8;
             this.label9.Text = "CategoriaId";
             // 
-            // textBox1
+            // PeliculasIdTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(111, 67);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(317, 20);
-            this.textBox1.TabIndex = 9;
+            this.PeliculasIdTextBox.Location = new System.Drawing.Point(111, 67);
+            this.PeliculasIdTextBox.Name = "PeliculasIdTextBox";
+            this.PeliculasIdTextBox.Size = new System.Drawing.Size(317, 20);
+            this.PeliculasIdTextBox.TabIndex = 9;
+            this.PeliculasIdTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // TituloTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(111, 96);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(395, 20);
-            this.textBox2.TabIndex = 10;
+            this.TituloTextBox.Location = new System.Drawing.Point(111, 96);
+            this.TituloTextBox.Name = "TituloTextBox";
+            this.TituloTextBox.Size = new System.Drawing.Size(395, 20);
+            this.TituloTextBox.TabIndex = 10;
             // 
-            // textBox3
+            // SignosisTextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(111, 127);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(395, 20);
-            this.textBox3.TabIndex = 11;
+            this.SignosisTextBox.Location = new System.Drawing.Point(111, 127);
+            this.SignosisTextBox.Name = "SignosisTextBox";
+            this.SignosisTextBox.Size = new System.Drawing.Size(395, 20);
+            this.SignosisTextBox.TabIndex = 11;
             // 
-            // textBox4
+            // CalificacionTextBox
             // 
-            this.textBox4.Location = new System.Drawing.Point(111, 162);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(395, 20);
-            this.textBox4.TabIndex = 12;
+            this.CalificacionTextBox.Location = new System.Drawing.Point(111, 162);
+            this.CalificacionTextBox.Name = "CalificacionTextBox";
+            this.CalificacionTextBox.Size = new System.Drawing.Size(395, 20);
+            this.CalificacionTextBox.TabIndex = 12;
             // 
-            // textBox5
+            // IdiomaTextBox
             // 
-            this.textBox5.Location = new System.Drawing.Point(111, 233);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(395, 20);
-            this.textBox5.TabIndex = 13;
+            this.IdiomaTextBox.Location = new System.Drawing.Point(111, 233);
+            this.IdiomaTextBox.Name = "IdiomaTextBox";
+            this.IdiomaTextBox.Size = new System.Drawing.Size(395, 20);
+            this.IdiomaTextBox.TabIndex = 13;
             // 
-            // textBox6
+            // GeneroTextBox
             // 
-            this.textBox6.Location = new System.Drawing.Point(111, 267);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(395, 20);
-            this.textBox6.TabIndex = 14;
+            this.GeneroTextBox.Location = new System.Drawing.Point(111, 267);
+            this.GeneroTextBox.Name = "GeneroTextBox";
+            this.GeneroTextBox.Size = new System.Drawing.Size(395, 20);
+            this.GeneroTextBox.TabIndex = 14;
             // 
-            // comboBox1
+            // AnnoComboBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(111, 195);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(395, 21);
-            this.comboBox1.TabIndex = 18;
+            this.AnnoComboBox.FormattingEnabled = true;
+            this.AnnoComboBox.Location = new System.Drawing.Point(111, 195);
+            this.AnnoComboBox.Name = "AnnoComboBox";
+            this.AnnoComboBox.Size = new System.Drawing.Size(395, 21);
+            this.AnnoComboBox.TabIndex = 18;
             // 
-            // comboBox2
+            // CategoriaIdComboBox
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(111, 298);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(395, 21);
-            this.comboBox2.TabIndex = 19;
+            this.CategoriaIdComboBox.FormattingEnabled = true;
+            this.CategoriaIdComboBox.Location = new System.Drawing.Point(111, 298);
+            this.CategoriaIdComboBox.Name = "CategoriaIdComboBox";
+            this.CategoriaIdComboBox.Size = new System.Drawing.Size(395, 21);
+            this.CategoriaIdComboBox.TabIndex = 19;
             // 
             // button5
             // 
@@ -202,6 +203,7 @@
             this.button5.Text = "Eliminar";
             this.button5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button4
             // 
@@ -225,6 +227,7 @@
             this.button3.Text = "Modificar";
             this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -236,6 +239,7 @@
             this.button2.Text = "Nuevo";
             this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -247,6 +251,7 @@
             this.button1.Text = "Buscar";
             this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // RegistroPelicula
             // 
@@ -257,15 +262,15 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.CategoriaIdComboBox);
+            this.Controls.Add(this.AnnoComboBox);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.GeneroTextBox);
+            this.Controls.Add(this.IdiomaTextBox);
+            this.Controls.Add(this.CalificacionTextBox);
+            this.Controls.Add(this.SignosisTextBox);
+            this.Controls.Add(this.TituloTextBox);
+            this.Controls.Add(this.PeliculasIdTextBox);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -293,15 +298,15 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox PeliculasIdTextBox;
+        private System.Windows.Forms.TextBox TituloTextBox;
+        private System.Windows.Forms.TextBox SignosisTextBox;
+        private System.Windows.Forms.TextBox CalificacionTextBox;
+        private System.Windows.Forms.TextBox IdiomaTextBox;
+        private System.Windows.Forms.TextBox GeneroTextBox;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox AnnoComboBox;
+        private System.Windows.Forms.ComboBox CategoriaIdComboBox;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
--- RegistroPeliculas/RegistroPeliculas/RegistroPelicula.cs
+++ RegistroPeliculas/RegistroPeliculas/RegistroPelicula.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +8,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Entidades;
 
 namespace RegistroPeliculas
 {
     public partial class RegistroPelicula : Form
     {
+        Pelicula pelicula = new Pelicula();
         public RegistroPelicula()
         {
             InitializeComponent();
@@ -19,7 +22,68 @@ namespace RegistroPeliculas
 
         private void button4_Click(object sender, EventArgs e)
         {
+            LlenarClase(pelicula);
 
+            PeliculasBll.Insertar(pelicula);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            LlenarClase(pelicula);
+
+            PeliculasBll.Eliminar(StringToInt(PeliculasIdTextBox.Text));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LlenaCampos(PeliculasBll.Buscar(StringToInt(PeliculasIdTextBox.Text)));
+        }
+        private void LlenaCampos(Pelicula pelicula)
+        {
+            PeliculasIdTextBox.Text = pelicula.PeliculasId.ToString();
+            TituloTextBox.Text = pelicula.Titulo;
+            SignosisTextBox.Text = pelicula.Signosis;
+            CalificacionTextBox.Text = pelicula.Calificacion.ToString();
+            AnnoComboBox.Text = pelicula.Anno.ToString();
+            IdiomaTextBox.Text = pelicula.Idioma;
+            GeneroTextBox.Text = pelicula.Genero;
+            CategoriaIdComboBox.Text = pelicula.CategoriaId.ToString();
+        }
+
+        private void LlenarClase(Pelicula pelicula)
+        {
+            pelicula.PeliculasId = StringToInt(PeliculasIdTextBox.Text);
+            pelicula.Titulo = TituloTextBox.Text;
+            pelicula.Signosis = SignosisTextBox.Text;
+            pelicula.Calificacion = StringToInt(CalificacionTextBox.Text);
+            pelicula.Anno = StringToInt(AnnoComboBox.Text);
+            pelicula.Idioma = IdiomaTextBox.Text;
+            pelicula.Genero = GeneroTextBox.Text;
+            pelicula.CategoriaId = (int)CategoriaIdComboBox.SelectedValue;
+        }
+        public int StringToInt(string texto)
+        {
+            int numero = 0;
+
+            int.TryParse(texto, out numero);
+
+            return numero;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LlenarClase(pelicula);
+
+           LlenaCampos(PeliculasBll.Modificar(StringToInt(PeliculasIdTextBox.Text)));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LlenarClase(pelicula);
         }
     }
 }
